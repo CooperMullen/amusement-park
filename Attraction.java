52bed7226a98229909bbfca3ae7b7e59498f4617
DanNguyen17 <75445727+DanNguyen17@users.noreply.github.com>
2020-12-03 11:29:00 -0700

    Update Attraction.java

--- Attraction.java
+++ Attraction.java
@@ -6,15 +6,12 @@ import java.util.Random;
 public abstract class Attraction {
 	private ArrayList<String> players = new ArrayList<>();
 	private ArrayList<String> deadPlayers = new ArrayList<>();
-	private int numPlayers;
+	private int numPlayers = players.size();
+	private int numDeadPlayers = deadPlayers.size();
 	private int maxPlayersPerRide;
 	private int risk;
 	
 	private Random rand = new Random();
-	
-	public Attraction(int numPlayers) {
-		this.numPlayers = numPlayers;
-	}
 	
 	public int getNumPlayers() {
 		return numPlayers;
@@ -25,15 +22,20 @@ public abstract class Attraction {
 	}
 	
 	public void calculateRisk(int numRisk) {
+		int pplDead = numPlayers * numRisk / 100;
+		
+		for (int i = 0; i<pplDead; i++) {
+			int deadPerson = rand.nextInt(numPlayers);
+			String deadPersonName = players.get(deadPerson);
+			
+			players.remove(deadPerson);
+			deadPlayers.add(deadPersonName);
+		}
 		
 	}
-	
-	public void playerDead(String name) {
-		
-	}
-	
-	public void addPlayers() {
-		for (int i = 1; i <= numPlayers; i++) {
+
+	public void addPlayers(int numberPlayers) {
+		for (int i = 1; i <= numberPlayers; i++) {
 			if (i < 10) 
 				players.add("Player " + 0 + i);
 			else 
@@ -58,8 +60,8 @@ public abstract class Attraction {
 		players.set(0, name);
 	}
 	
-	public abstract String toString();
-	
 	public abstract void runAttraction();
+	
+	public abstract String toString();
 	
 }
